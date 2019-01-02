2013ee3996fb4dd610e39ef5d4b5b2e3cece09a6
Ruthild <rg008b@gmail.com>
2019-01-02 10:19:03 +0100

    Added comments

--- app/src/main/java/com/example/robin/angrynerds_wip/data/services/Create.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/services/Create.java
@@ -5,11 +5,10 @@ import com.example.robin.angrynerds_wip.data.models.tens.Note;
 import com.example.robin.angrynerds_wip.data.models.tens.Todo;
 
 public class Create {
-
-    /*--------------------
-    Ruthild Gilles (04.12.2018)
-    Klasse für Activities zum Erstellen von neuen leeren TEN Objekten
-     */
+    /* Ruthild Gilles (30.11.2018)
+     Class Create contains methods to create new empty TEN objects.
+     This class only exists to give a consistent form to the CRUD methods.
+    */
 
     public static Todo newTodo() {
         return new Todo();
