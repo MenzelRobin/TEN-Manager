464e5db5405490cc892bfc7aad17afaf73b6fb42
Jan Beilfuß <jan.beilfuss@web.de>
2019-01-08 00:07:37 +0100

    good Mock Data

--- app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/MockData.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/MockData.java
@@ -8,6 +8,7 @@ import com.example.robin.angrynerds_wip.data.models.tens.Note;
 import com.example.robin.angrynerds_wip.data.models.tens.TEN;
 import com.example.robin.angrynerds_wip.data.models.tens.Todo;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -20,46 +21,202 @@ public class MockData {
     public ArrayList<TEN> tenMockData = new ArrayList<>();
 
     public MockData() {
-        int[] bgColors = new int[]{
-                Color.parseColor("#DD2C00"),
-                Color.parseColor("#087f23"),
-                Color.parseColor("#00766c"),
-                Color.parseColor("#3d5afe"),
-                Color.parseColor("#1a237e"),
-                Color.parseColor("#b7008a"),
-                Color.parseColor("#ffab00"),
-                Color.parseColor("#f57f17"),
-                Color.parseColor("#60e10f"),
-                Color.parseColor("#8c234d")
-        };
 
-        ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Task 1", false));
-        tasks.add(new Task("Task 2", true));
-        tasks.add(new Task("Task 3", false));
+        Note koalaNote = new Note("Interessantes über Koalabären", "Der Koala ist ein " +
+                "baumbewohnender Beutelsäuger in Australien. Er wurde von dem Zoologen Georg " +
+                "August Goldfuß im Jahre 1817 als Lipurus cinereus erstbeschrieben. Der Koala ist " +
+                "neben dem Känguru das am weitesten verbreitete Symbol Australiens.");
 
-        ArrayList<Date> reminder = new ArrayList<>();
-        reminder.add(new Date());
-        reminder.add(new Date(1540942029));
+        ArrayList<String> koalaTags = new ArrayList<String>();
+        koalaTags.add("Bär");
+        koalaTags.add("Baum");
+        koalaTags.add("Eukalyptus");
+        koalaTags.add("Australien");
+        //TODO Bilder
+        koalaNote.setTags(koalaTags);
+        tenMockData.add(koalaNote);
 
-        ArrayList<String> tags = new ArrayList<>();
-        tags.add("Tag1");
-        tags.add("Tag2");
-        tags.add("Tag3");
+        Date visitKoalaDate = new Date(1588590000);
+        Date reminder2 = new Date(1588590000 - (1000 * 60 * 60 * 24 * 7));
+        ArrayList<Date> visitKoalaReminder = new ArrayList<Date>();
+        visitKoalaReminder.add(visitKoalaDate);
+        visitKoalaReminder.add(reminder2);
+        String address = "Australien, Outback";
+        Event koalaEvent = new Event("Koalas besuchen", visitKoalaDate, visitKoalaReminder, address);
+        tenMockData.add(koalaEvent);
 
-        RecurringType[] recurringTypes = new RecurringType[]{
-                RecurringType.NONE,
-                RecurringType.DAILY,
-                RecurringType.WEEKLY,
-                RecurringType.MONTHLY,
-                RecurringType.YEARLY
-        };
+        Note delfinNote = new Note("Delfine", "Die Delfine oder Delphine gehören zu " +
+                "den Zahnwalen und sind somit Säugetiere, die im Wasser leben. Delfine sind die" +
+                " vielfältigste und mit rund 40 Arten größte Familie der Wale. Sie sind in allen " +
+                "Meeren verbreitet.");
 
-        for (int i = 1; i <= 10; i++) {
-            this.tenMockData.add(new Todo("Todo " + i, bgColors[i - 1], "This is a toDo.", tasks, new Date()));
-            this.tenMockData.add(new Event("Event " + i, bgColors[i - 1], Calendar.getInstance().getTime(), reminder,
-                    "Schlossallee 1, 50764 Köln", recurringTypes[i % 5]));
-            this.tenMockData.add(new Note("Note " + i, bgColors[i - 1], "This is a note.", tags));
-        }
+        ArrayList<String> delfinTags = new ArrayList<String>();
+        koalaTags.add("Säugetiere");
+        koalaTags.add("intelligent");
+        koalaTags.add("Meer");
+        delfinNote.setTags(delfinTags);
+        tenMockData.add(delfinNote);
+
+
+        Task besuchMamaPapa1 = new Task("Küche putzen");
+        Task besuchMamaPapa2 = new Task("Bad putzen");
+        Task besuchMamaPapa3 = new Task("Zimmer aufräumen");
+
+        ArrayList<Task> besuchMamaPapaTasks = new ArrayList<Task>();
+        besuchMamaPapaTasks.add(besuchMamaPapa1);
+        besuchMamaPapaTasks.add(besuchMamaPapa2);
+        besuchMamaPapaTasks.add(besuchMamaPapa3);
+
+        Todo besuchMamaPapa = new Todo("Besuch von Mama und Papa", "Alles muss sauber und " +
+                "aufgeräumt sein!", besuchMamaPapaTasks);
+        tenMockData.add(besuchMamaPapa);
+
+        Note klausureingrenzung = new Note("Klausureingrenzung", "Es kommt die folgenden Folien vor: 24-29, 50-71, 83-101");
+        ArrayList<String> klausurTags = new ArrayList<String>();
+        klausurTags.add("wichtig");
+        klausurTags.add("FH");
+        klausureingrenzung.setTags(klausurTags);
+        tenMockData.add(delfinNote);
+
+
+        Date konzertBringsDate = new Date(1563879600);
+        Date reminder2Brings = new Date(1563879600 - (1000 * 60 * 60 * 24 * 7));
+        ArrayList<Date> konzertBringsReminder = new ArrayList<Date>();
+        konzertBringsReminder.add(konzertBringsDate);
+        konzertBringsReminder.add(reminder2Brings);
+        String addressBrings = "Köln, Tanzbrunnen";
+        Event konzertBrings = new Event("Konzert Brings", konzertBringsDate, konzertBringsReminder, addressBrings);
+        tenMockData.add(konzertBrings);
+
+        Date elternBesuchDate = new Date(1550826000);
+        Date reminder2elternBesuch = new Date(1550826000 - (1000 * 60 * 60 * 24 * 7));
+        ArrayList<Date> elternBesuchReminder = new ArrayList<Date>();
+        elternBesuchReminder.add(elternBesuchDate);
+        elternBesuchReminder.add(reminder2elternBesuch);
+        String addressElternBesuch = "zuhause";
+        Event elternBesuch = new Event("Besuch von Mama und Papa", elternBesuchDate, elternBesuchReminder, addressElternBesuch);
+        tenMockData.add(elternBesuch);
+
+
+        Task küchePutzen1 = new Task("Arbeitsfläche wischen");
+        Task küchePutzen2 = new Task("Geschirr spülen");
+        Task küchePutzen3 = new Task("Gläser einräumen");
+
+        ArrayList<Task> küchePutzenTasks = new ArrayList<Task>();
+        küchePutzenTasks.add(küchePutzen1);
+        küchePutzenTasks.add(küchePutzen2);
+        küchePutzenTasks.add(küchePutzen3);
+
+        Todo küchePutzen = new Todo("Küche aufräumen", "Das Backen war ein Fiasko",
+                küchePutzenTasks);
+        tenMockData.add(küchePutzen);
+
+
+        Task reiseVorbereitung1 = new Task("Wäsche waschen");
+        Task reiseVorbereitung2 = new Task("Taschepacken");
+        Task reiseVorbereitung3 = new Task("Zeitschaltuhr einstellen");
+
+        ArrayList<Task> reiseVorbereitungTasks = new ArrayList<Task>();
+        reiseVorbereitungTasks.add(reiseVorbereitung1);
+        reiseVorbereitungTasks.add(reiseVorbereitung2);
+        reiseVorbereitungTasks.add(reiseVorbereitung3);
+
+        Todo reiseVorbereitung = new Todo("Vorbereitung auf die Australienreise",
+                "",
+                reiseVorbereitungTasks);
+        tenMockData.add(reiseVorbereitung);
+
+        Note spinnenNote = new Note("Giftige Spinnen","Funnel-Web Spider,\n Wolf Spider,\n Huntsman");
+        ArrayList<String> spinnenTags = new ArrayList<String>();
+        spinnenTags.add("giftig");
+        spinnenTags.add("gefährlich");
+        spinnenTags.add("Eukalyptus");
+        spinnenTags.add("Australien");
+        //TODO Bilder
+        spinnenNote.setTags(spinnenTags);
+        tenMockData.add(spinnenNote);
+
+        Note melbourneNote = new Note("Melbourne", "Melbourne ist die an der " +
+                "Südostküste Australiens gelegene Hauptstadt des australischen Bundesstaats " +
+                "Victoria. Im Stadtzentrum liegt der moderne Federation-Square-Komplex mit Plätzen," +
+                " Bars und Restaurants am Fluss Yarra. Am südlichen Flussufer im Kunstbezirk der" +
+                " Stadt befinden sich das Arts Centre Melbourne – ein Zentrum für darstellende" +
+                " Künste – und die National Gallery of Victoria, in der Werke von australischen" +
+                " Künstlern und Aborigines ausgestellt werden.");
+        ArrayList<String> melbourneTags = new ArrayList<String>();
+        melbourneTags.add("City");
+        melbourneTags.add("Nightlife");
+        melbourneTags.add("River");
+        melbourneTags.add("Skyline");
+        //TODO Bilder
+        melbourneNote.setTags(melbourneTags);
+        tenMockData.add(melbourneNote);
+
+        Note bambusNote = new Note("Bambus", "ambus ist eine der zwölf" +
+                " Unterfamilien aus der Familie der Süßgräser, der etwa 116 Gattungen zugerechnet" +
+                " werden. Die Unterfamilie wird in drei Tribus geteilt, wobei Arundinarieae und" +
+                " Bambuseae verholzende Arten beinhalten und Olyreae krautig wachsende Pflanzen.");
+        ArrayList<String> bambusTags = new ArrayList<String>();
+        bambusTags.add("Bär");
+        bambusTags.add("Baum");
+        bambusTags.add("Eukalyptus");
+        bambusTags.add("Australien");
+        //TODO Bilder
+        bambusNote.setTags(bambusTags);
+        tenMockData.add(bambusNote);
+
+        Date delfinDate = new Date(1588831200);
+        Date reminder2delfin = new Date(1588831200 - (1000 * 60 * 60 * 24 * 7));
+        ArrayList<Date> delfinReminder = new ArrayList<Date>();
+        delfinReminder.add(delfinDate);
+        delfinReminder.add(reminder2delfin);
+        String delfinAdresse = "Tin Can Bay";
+        Event delfinSchwimmen = new Event("Schwimmen mit Delfinen", delfinDate, delfinReminder, delfinAdresse);
+        tenMockData.add(delfinSchwimmen);
+
+
+        Task klausurVorbereitung1 = new Task("24-29 lernen");
+        Task klausurVorbereitung2 = new Task("50-71 lernen");
+        Task klausurVorbereitung3 = new Task("83-101 lernen");
+        Task klausurVorbereitung4 = new Task("Übungsaufgaben Bilanz");
+        Task klausurVorbereitung5 = new Task("Übungsaufgaben GuV");
+
+        ArrayList<Task> klausurVorbereitungTasks = new ArrayList<Task>();
+        klausurVorbereitungTasks.add(klausurVorbereitung1);
+        klausurVorbereitungTasks.add(klausurVorbereitung2);
+        klausurVorbereitungTasks.add(klausurVorbereitung3);
+        klausurVorbereitungTasks.add(klausurVorbereitung4);
+        klausurVorbereitungTasks.add(klausurVorbereitung5);
+
+        Todo klausurVorbereitung = new Todo("Vorbereitung auf die Rechnungswesenklausur",
+                "",
+                klausurVorbereitungTasks);
+        tenMockData.add(klausurVorbereitung);
+
+
+        Task appProgrammierung1 = new Task("Anforderungen analysieren", true);
+        Task appProgrammierung2 = new Task("Projekt planen", true);
+        Task appProgrammierung3 = new Task("App entwerfen", true);
+        Task appProgrammierung4 = new Task("App implementieren", true);
+        Task appProgrammierung5 = new Task("App testen", true);
+        Task appProgrammierung6 = new Task("App übergeben");
+
+        ArrayList<Task> appProgrammierungTasks = new ArrayList<Task>();
+        appProgrammierungTasks.add(appProgrammierung1);
+        appProgrammierungTasks.add(appProgrammierung2);
+        appProgrammierungTasks.add(appProgrammierung3);
+        appProgrammierungTasks.add(appProgrammierung4);
+        appProgrammierungTasks.add(appProgrammierung5);
+        appProgrammierungTasks.add(appProgrammierung6);
+
+        Todo appProgrammierung = new Todo("App Programmierung",
+                "Grobes Schema einer Softwareentwickung",
+                appProgrammierungTasks);
+        tenMockData.add(appProgrammierung);
     }
+
+    public ArrayList<TEN> getMockData() {
+        return this.tenMockData;
+    }
+
 }
