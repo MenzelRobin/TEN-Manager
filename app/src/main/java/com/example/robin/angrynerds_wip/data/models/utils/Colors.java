52751de4c50740619626e21d1036bcdd19a8c173
MenzelRobin <robin.menzel@ymail.com>
2019-01-20 23:54:23 +0100

    Added Notification Service and AlarmManager for Reminder

--- app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/Colors.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/models/utils/Colors.java
@@ -7,16 +7,24 @@ public class Colors {
 
             0xFF0288D1, //light blue
             0xFFD32F2F, //red
-            0xFF616161, //gray
+            0xFFAFB42B, //lime
             0xFF388E3C, //green
+            0xFFEF6C00, //Orange
+            0xFF512DA8, //Deep Purple
+            0xFFC2185B, //Pink
+            0xFF00796B, //Teal
     };
 
     //900er Material Colors
     public static final int[] DARKER_ACCENT_COLORS = {
             0xFF01579B, //light blue
             0xFFB71C1C, //red
-            0xFF212121, //gray
+            0xFF827717, //lime
             0xFF1B5E20, //green
+            0xFFE65100, //orange
+            0xFF311B92, //Deep Purple
+            0xFF880E4F, //Pink
+            0xFF004D40, //Teal
     };
 
     public static int getRandomColorIndex() {
