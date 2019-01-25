3cfabfba9d3cdc12415677b383cdc24ff8f467af
Yannick Rüttgers <yannick-luca.ruettgers@gmx.de>
2019-01-25 19:15:12 +0100

    Added OverviewImage Base

--- app/src/main/java/com/example/robin/angrynerds_wip/overview/imageFragment/OverviewImageData.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/overview/imageFragment/OverviewImageData.java
@@ -1,4 +1,21 @@
 package com.example.robin.angrynerds_wip.overview.imageFragment;
 
-public class OverviewImageData {
+import android.os.Bundle;
+
+import com.example.robin.angrynerds_wip.overview.superClasses.OverviewFragmentData;
+
+public class OverviewImageData extends OverviewFragmentData {
+
+    String mDescription;
+
+    // Adds the Note data to this Object
+    public void addData(Bundle pData){
+        super.addData(pData);
+        mDescription = pData.getString("Description");
+    }
+
+    // Returns the Description
+    public String getDescription(){
+        return mDescription;
+    }
 }
--- app/src/main/java/com/example/robin/angrynerds_wip/overview/imageFragment/OverviewImageGui.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/overview/imageFragment/OverviewImageGui.java
@@ -1,4 +1,44 @@
 package com.example.robin.angrynerds_wip.overview.imageFragment;
 
-public class OverviewImageGui {
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.example.robin.angrynerds_wip.R;
+import com.example.robin.angrynerds_wip.overview.superClasses.OverviewFragmentGui;
+
+public class OverviewImageGui extends OverviewFragmentGui {
+
+    private RelativeLayout mOverviewImage;
+    //private TextView mTitle;
+    //private TextView mDescription;
+
+    // Adds the Fragment view to this Object
+    public void addView(View pView){
+        super.addView(pView);
+        mChecked = mView.findViewById(R.id.id_overview_note_imageView_checked);
+        mUnchecked = mView.findViewById(R.id.id_overview_note_imageView_unchecked);
+        mOverviewImage = mView.findViewById(R.id.id_overview_image);
+        //mTitle = mView.findViewById(R.id.id_overview_note_textView_title);
+        //mDescription = mView.findViewById(R.id.id_overview_note_textView_description);
+    }
+
+    // Returns mOverviewImage
+    public RelativeLayout getOverviewImage(){
+        return mOverviewImage;
+    }
+
+    // Sets Title
+    //public void setTitle(String pTitle){
+    //    mTitle.setText(pTitle);
+    //}
+
+    // Sets Color
+    public void setColor(int pColor){
+        mOverviewImage.setBackgroundColor(pColor);
+    }
+
+    // Sets Description
+    //public void setDescription(String pDescription){
+    //    mDescription.setText(pDescription);
+    //}
 }
