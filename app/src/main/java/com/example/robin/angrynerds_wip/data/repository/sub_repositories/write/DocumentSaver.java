fa13568f41c4e310ee2c254faff00a964a9c8c41
jaxefoot <42271695+jaxefoot@users.noreply.github.com>
2019-02-05 19:50:28 +0100

    Revert "commented and renamed Document Saver"
    This reverts commit cd242082b602cf5af0e7af9ccf147d677dc5e04d.

--- app/src/main/java/com/example/robin/angrynerds_wip/data/repository/sub_repositories/write/DocumentSaver.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/data/repository/sub_repositories/write/DocumentSaver.java
@@ -2,12 +2,16 @@ package com.example.robin.angrynerds_wip.data.repository.sub_repositories.write;
 
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.MutableDocument;
+import com.example.robin.angrynerds_wip.data.models.tens.Note;
 import com.example.robin.angrynerds_wip.data.models.tens.TEN;
+import com.example.robin.angrynerds_wip.data.models.utils.Image;
 import com.example.robin.angrynerds_wip.data.repository.DataContextManager;
 import com.example.robin.angrynerds_wip.data.repository.RepositoryConstants;
 import com.example.robin.angrynerds_wip.data.repository.filesystem.FileRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 //class that manages the persistent saving process of Tens
 //Author: Jan Beilfuß
@@ -21,9 +25,8 @@ public class DocumentSaver {
         this.mFileRepository = new FileRepository();
     }
 
-    //manages process of writing a ten to a document and saving this
     public boolean updateCompleteDocument(TEN pTen, MutableDocument pMutableTENDocument) {
-        pMutableTENDocument = setTypeOfDocument(pMutableTENDocument, pTen);
+        pMutableTENDocument = preparePersistentSaving(pMutableTENDocument, pTen);
         pMutableTENDocument = saveSeparateAttributes(pMutableTENDocument, pTen);
         try {
             pMutableTENDocument.setString(RepositoryConstants.OBJECT_KEY, this.mObjectMapper.writeValueAsString(pTen));
@@ -38,7 +41,6 @@ public class DocumentSaver {
         }
     }
 
-    //saves attributes not saved in the JSON-String
     private MutableDocument saveSeparateAttributes(MutableDocument pMutableDocument, TEN pTen){
         pMutableDocument.setDate(RepositoryConstants.CREATION_DATE_KEY, pTen.getDateOfCreation());
         pMutableDocument.setInt(RepositoryConstants.COLOR_KEY, pTen.getColor());
@@ -47,8 +49,7 @@ public class DocumentSaver {
         return pMutableDocument;
     }
 
-    //Sets Type depending of the class of the given Object
-    private MutableDocument setTypeOfDocument(MutableDocument pMutableDocument, TEN pTen) {
+    private MutableDocument preparePersistentSaving(MutableDocument pMutableDocument, TEN pTen) {
 
         if (pTen.getClass().getName().contains("Event")) {
             pMutableDocument.setString(RepositoryConstants.TYPE_KEY, RepositoryConstants.EVENT_TYPE);
