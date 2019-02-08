d0013d99540d924c1ca77594f9a804c0408fc056
Yannick Rüttgers <yannick-luca.ruettgers@gmx.de>
2019-02-08 20:52:07 +0100

    added birdie

--- app/src/main/java/com/example/robin/angrynerds_wip/MainActivity.java
+++ app/src/main/java/com/example/robin/angrynerds_wip/MainActivity.java
@@ -6,16 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.robin.angrynerds_wip.data.models.tens.TEN;
 import com.example.robin.angrynerds_wip.data.models.utils.Colors;
 import com.example.robin.angrynerds_wip.data.models.utils.MockData;
-import com.example.robin.angrynerds_wip.data.repository.DatabaseRepository;
 import com.example.robin.angrynerds_wip.data.repository.DataContextManager;
 import com.example.robin.angrynerds_wip.data.repository.sub_repositories.read.GetAllTensQuery;
 import com.example.robin.angrynerds_wip.data.services.Delete;
 import com.example.robin.angrynerds_wip.activities.overview.overviewActivity.OverviewInit;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     /* Yannick-Luca Ruettgers
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setColor();
+        setImage();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
             @Override
@@ -36,21 +39,29 @@ public class MainActivity extends AppCompatActivity {
                 if(!mLoading) startOverview();
             }
         }, 2000);
+    }
 
+    @Override
+    public void onBackPressed(){
+        mLoading = true;
+        super.onBackPressed();
     }
 
     public void onClickSkip(View view){
-        mLoading = true;
-        startOverview();
+        if(!mLoading) {
+            mLoading = true;
+            startOverview();
+        }
     }
 
     public void onClickReset(View view){
-        mLoading = true;
-        Toast.makeText(getApplicationContext(), "Resetting Data...", Toast.LENGTH_SHORT).show();
-        DataContextManager.initDatabase(getApplicationContext());
-        resetDatabase();
-        addTestData();
-        startOverview();
+        if(!mLoading) {
+            mLoading = true;
+            DataContextManager.initDatabase(getApplicationContext());
+            resetDatabase();
+            addTestData();
+            startOverview();
+        }
     }
 
     public void addTestData(){
@@ -76,7 +87,11 @@ public class MainActivity extends AppCompatActivity {
     public void setColor(){
         int colorIndex = Colors.getRandomColorIndex();
         int color = Colors.COLORS[colorIndex];
-        this.findViewById(R.id.id_main_linearLayout_skip).setBackgroundColor(color);
-        this.findViewById(R.id.id_main_linearLayout_reset).setBackgroundColor(color);
+        this.findViewById(R.id.id_main_relativeLayout_main).setBackgroundColor(color);
+    }
+
+    public void setImage(){
+        int[] birbs = {R.drawable.yelowbird_scaled_transparent, R.drawable.flapybird_scaled_transparent};
+        ((ImageView) this.findViewById(R.id.id_main_imageView_image)).setImageResource(birbs[new Random().nextInt(birbs.length)]);
     }
 }
